1a1af92aa944a153386da68a736e89acd59eca4c
alexxx-ivanow <alexxx-ivanow@yandex.ru>
2018-07-22 03:00:49 +0300

    Маленькие допилы с роутами при аутентификации и разлогинивании

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -27,7 +27,35 @@
                     <h5>Сервисы</h5>
                 </a>
             </li>
+
+            <li class="nav navbar-nav navbar-right admin-exit">
+                <div class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
         </ul>
+
+
+
     </div>
+
+
 
 </div>
