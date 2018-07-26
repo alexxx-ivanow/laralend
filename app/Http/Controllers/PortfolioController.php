01b7d661f003a6ee253198c8c810a1cfaf967756
alexxx-ivanow <alexxx-ivanow@yandex.ru>
2018-07-27 02:27:47 +0300

    Добавил в админке страницу вывода списка фото портфолио

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Portfolio;
 
 class PortfolioController extends Controller
 {
     //
+    public  function execute() {
+
+        $value = Portfolio::all()->toArray();
+
+        if (view()->exists('admin.portfolios')) {
+            return view('admin.portfolios', ['value' => $value])->withTitle('Раздел портфолио');
+        }
+
+    }
 }
---
+++ resources/views/admin/content_portfolios.blade.php
@@ -0,0 +1,51 @@
+<div class="serviceWrapContent container">
+
+    @if($value)
+
+        <h3>Список фото для портфолио</h3>
+
+        <div class="servicesItem">
+            <div class="serviceInWrap" style="color: green">
+
+                <div class="serviceItemName col-xs-4"><h4>Фото</h4></div>
+                <div class="serviceItemNum col-xs-3"><h4>Название</h4></div>
+                <div class="serviceItemText col-xs-5"><h4>Категория фото</h4></div>
+            </div>
+        </div>
+
+
+        @foreach($value as $k => $item)
+
+            <div class="servicesItem">
+                <div class="serviceInWrap">
+
+                    <div class="serviceItemNum col-xs-4">
+                        @if (!empty($item['images']))
+
+                            <div class="img-portfolio">
+                                {!! Html::image('assets/img/'.$item['images'], 'Фото для портфолио', ['class' => 'imgP']) !!}
+                            </div>
+
+                        @endif
+
+                     </div>
+                    <div class="serviceItemName col-xs-3"><a href="{{route('portfolioEdit',[$item['id']])}}">{{ $item['name'] }}</a></div>
+                    <div class="serviceItemText col-xs-3">{!! $item['filter'] !!}</div>
+                    <div class="serviceItemDel col-xs-2">
+                        {!! Form::open(['url'=>route('portfolioEdit',['portfolio'=>$item['id']]), 'class'=>'form-horizontal','method' => 'POST']) !!}
+                        {!! Form::hidden('id', $item['id']) !!}
+                        {{ method_field('DELETE') }}
+                        {!! Form::button('Удалить',['class'=>'btn btn-danger','type'=>'submit']) !!}
+
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        @endforeach
+
+    @else
+        {!!  '<h2>Пока у вас нет услуг...</h2>' !!}
+    @endif
+
+    {!! Html::link(route('portfolioAdd'),'Добавить новое фото в портфолио') !!}
+</div>
